177c9b38ef93024e00d947f60c4c25ec0932ed14
Dan Pham <phamphuc0603@gmail.com>
2022-08-31 19:25:39 +0700

    Bài 7: Cấu trúc rẽ nhánh - If else - Bài tập

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,36 @@ namespace LearningProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("Hello moto");
+            
         }
+
+        /*  Bài 1:
+         *  Nhập 2 số A B bất kì
+         *  Kiểm tra xem số A có chia hết cho số B hay là ko? // Toán tử, toán tử toán học
+         *  Nếu có -> A chia hết cho B
+         *  Nếu ko -> A ko chia hết cho B
+         * 
+         * 
+         *  Bài 2:
+         *  Nhập 1 năm bất kì từ 0 -> 5000 
+         *  Kiểm tra xem năm đó có phải là năm nhuận hay ko? 4 100 400
+         *  Nếu là năm nhuận thì xuất ra 
+         *  Năm {x} có 366 ngày
+         *  Nếu ko phải thì xuất ra
+         *  Năm {x} có 365 ngày
+         *  
+         *  Bài 3:
+         *  Nhập vào điểm 3 môn học. Toán Văn Anh
+         *  Kiểm tra học lực của người dùng biết rằng
+         *  ĐTB >= 8.5 là Giỏi
+         *  ĐTB >= 6.5 là Khá
+         *  ĐTB >= 5 là Trung bình
+         *  Bonus: ko có môn nào được dưới ngưỡng (có thể bỏ nếu quá khó) 
+         * 
+         *  Bài 4:
+         *  Nhập vào 3 số  a b c. Đại diện cho phương trình bậc 2  ax^2 + bx + c = 0
+         *  Tìm x
+         * 
+         */
     }
 }
