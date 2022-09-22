f8a58110e729129c394063026d33c2ed6eb9190f
Dan Pham <phamphuc0603@gmail.com>
2022-09-22 19:06:49 +0700

    Bài 16: Bài tập tổng hợp #1

--- Program.cs
+++ Program.cs
@@ -9,23 +9,27 @@ namespace LearningProject
         static void Main(string[] args)
         {
             /*
-            Bài 1: Viết chương trình nhập vào 1 mảng số nguyên có N phần tử
-            (N do người dùng nhập vào)
-            rồi xuất các phần tử mảng này trên màn hình. 
+             Bài 1:
+                Viết hàm kiểm tra số nguyên tố
+             
+             Bài 2:
+                Viết hàm xuất ra N số nguyên tố đầu tiên (N được nhập vào)
 
-            Bài 2: Y như bài 1 nhưng xuất mảng theo chiều ngược
+             Bài 3:
+                Viết hàm kiểm tra số hoàn thiện
 
-            Bài 3: Viết chương trình tính tổng tất cả các phần tử có trong mảng
-            Hướng dẫn:
-                B1: Nhập số lượng phần tử - sau đó đưa dữ liệu vào (bài 1)
-                B2: Viết hàm tính tổng tất cả các phần tử có trong mảng bằng for
+             Bài 4:
+                Viết hàm tính tổng các chữ số trong 1 số bất kì.
+                vd:
+                    102 -> 1+0+2 = 3
+                    548 -> 5+4+8 = 17
+             Bài 5:
+                Viết hàm tính tổng tất cả các số nguyên từ A -> B (A và B được nhập vào)
+                Sau đó hiển thị ra màn hình tổng các số từ a tới b
 
-            Bài 4: Viết hàm tìm phần tử lớn và nhỏ nhất trong mảng
-             
-             
-             
-             
-            */
+             Bài 6:
+                Viết hàm xuất ra N số fibonacci đầu tiên (N được nhập vào)
+             */
 
             Console.ReadLine();
         }
