f532e6e72e8d965b87536febcd5d3b977b484780
Dan Pham <phamphuc0603@gmail.com>
2022-09-27 19:35:59 +0700

    Xong bai1: bài tập tổng hợp 2

--- Program.cs
+++ Program.cs
@@ -9,24 +9,41 @@ namespace LearningProject
             /*
              Bài 1:
                 Viet hàm Nhập và Xuất 1 mảng số nguyên
-
-             Bài 2:
-                Viết hàm TÌM ra 1 số nguyên có trong mảng 
-
-             Bài 3:
-                Viết hàm xuất ra tất cả các số dương có trong mảng
-
-             Bài 4:
-                Viết hàm xuất ra tất cả các số chia hết cho N (N lấy từ người dùng)
-
-             Bài 5:
-                Viết hàm nhân tất cả các số chẵn có trong mảng lên xN (với N lấy từ người dùng)
-
-             Bài 6:
-                Viết hàm sắp xếp mảng tăng dần hoặc giảm dần
              */
 
+            int[] mang = NhapMangSoNguyen();
+            XuatMangSoNguyen(mang);
+
+
             Console.ReadLine();
+        }
+
+        // Viết hàm nhập, có output là mảng số nguyên, không có input
+        static int[] NhapMangSoNguyen()
+        {
+            int n;
+            Console.Write("Vui long nhap so luong phan tu co trong mang: ");
+            n = int.Parse(Console.ReadLine()); //4
+
+            int[] mangKetQua = new int[n];
+            for(int i = 0;i< mangKetQua.Length; i++) // 0  1 2 3 
+            {
+                Console.Write($"Vi tri {i} la: ");
+                mangKetQua[i] = int.Parse(Console.ReadLine());
+            }
+
+            return mangKetQua;
+        }
+
+        // Viết hàm xuất, ko có output, input sẽ là 1 mảng số nguyên
+
+        static void XuatMangSoNguyen(int[] mang)
+        {
+            Console.WriteLine($"Mang nay co {mang.Length} phan tu");
+            for (int i = 0; i < mang.Length; i++) // 0  1 2 3 
+            {
+                Console.WriteLine($"Vi tri {i} la: {mang[i]}");
+            }
         }
     }
 }
